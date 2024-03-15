919c922f6ae165c1a108c12156cd11437a328690
Arthur Sousa <106563296+ArthurMSousa@users.noreply.github.com>
2024-03-15 17:49:41 -0300

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
-//let title = document.querySelector('h1');
-//title.innerHTML = 'Jogo do número secreto';
-
-//let paragraph = document.querySelector('p');
-//paragraph.innerHTML = 'Escolha um número entre 1 a 10';
+let secretNumber = generateRandomNumber();
+let attempts = 1;
 
 function displayText(tag, text) {
     let space = document.querySelector(tag);
     space.innerHTML = text;  
 }
 
-displayText('h1', 'Jogo do número secreto');
-displayText('p', 'Escolha um número entre 1 e 100');
+function displayInitialMessage() {
+    displayText('h1', 'Jogo do número secreto');
+    displayText('p', 'Escolha um número entre 1 e 10');
+}
+
+displayInitialMessage();
 
 function verifyGuess() {
+    let guess = document.querySelector('input').value;
+    
+    if (guess == secretNumber) {
+        displayText('h1', 'Acertou!');
+        let attemptsWord = attempts > 1 ? 'tentativas' : 'tentativa';
+        let attemptsMessage = `Você descobriu o número secreto com ${attempts} ${attemptsWord}!`;
+        displayText('p', attemptsMessage);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (guess > secretNumber) {
+            displayText('p', 'O número secreto é menor');
+        } else {
+            displayText('p', 'O número secreto é maior');
+        }
+        attempts++;
+        clearSpace();
+    }
+}
 
+function generateRandomNumber() {
+    return parseInt(Math.random() * 10 + 1);  
+} 
+
+function clearSpace() {
+    guess = document.querySelector('input');
+    guess.value = '';
+}
+
+function newGame() {
+    secretNumber = generateRandomNumber();
+    clearSpace();
+    attempts = 1;
+    displayInitialMessage();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
